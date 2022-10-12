324f2c046fd939c6126c43223a9ff707053fdbfa
Mario Zenuni <mariozenuni@gm,ail>
2022-10-12 21:12:48 +0100

    added filter

--- resources/views/rubrica/index.blade.php
+++ resources/views/rubrica/index.blade.php
@@ -12,13 +12,13 @@
  
     <h5 class="card-title"></h5>
      <input  id="myInput" type="text" placeholder="Cerca profilo..">   
-          <a href="{{route('rubrica.create')}}"  class="btn btn-success float-end">Add User</a>
-    <table class="table table-striped">
+          <a href="{{route('rubrica.create')}}"  class="btn btn-success float-end">Add User</a><br><br>
+    <table  id="selectedColumn" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
             <thead>
-            <th>Nome  Cognome</th>
-            <th>E-mail</th>
-              <th>Phone/Mobile</th>
-              <th class="text-justify">Actions</th>
+            <th class="th-sm">Nome  Cognome</th>
+            <th class="th-sm">E-mail</th>
+              <th class="th-sm">Phone/Mobile</th>
+              <th class="text-justify th-sm">Actions</th>
             </thead>
                 <tbody id="myTable">
                   @foreach($rubricas as $rubrica)
@@ -33,11 +33,11 @@
                           {{ $rubrica->phone }} 
                     </td>
                          <td>
-                            <a href="{{route('rubrica.profile.edit',$rubrica->id)}}" class="btn btn-info btn-sm">Modifica</i></a><br>
+                            <a href="{{route('rubrica.profile.edit',$rubrica->id)}}" class="btn btn-info " id="modifica">Modifica</i></a><br>
                               <form action="{{route('rubrica.profile.delete',$rubrica->id)}}" method="post">
                                 @method('DELETE')
                                  @csrf
-                             <a href="{{ route('rubrica.profile.delete',$rubrica->id) }}" class="btn btn-danger btn-sm">Elimina</a>
+                             <a href="{{ route('rubrica.profile.delete',$rubrica->id) }}" class="btn btn-danger " id="elimina">Elimina</a>
                              </form>
                          </td> 
 
@@ -58,6 +58,7 @@
 
 @endsection
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+ <script src="js/jquery.dataTables.js" type="text/javascript"></script>
 <script>
 $(document).ready(function(){
   $("#myInput").on("keyup", function() {
@@ -66,6 +67,43 @@ $(document).ready(function(){
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
+  $('#selectedColumn').DataTable();
+  $('.dataTables_length').addClass('bs-select');
 });
 </script>
+
+
+
+<style>
+
+    #modifica
+    {
+    margin-left: 86px;
+    position: relative;
+    left: -74px;
+   
+    }
+
+    #elimina
+    {
+    margin-top  : 1px;
+     position: relative;
+       top: -37px;
+       right: -102px;
+    }
+
+table.dataTable thead .sorting:after,
+table.dataTable thead .sorting:before,
+table.dataTable thead .sorting_asc:after,
+table.dataTable thead .sorting_asc:before,
+table.dataTable thead .sorting_asc_disabled:after,
+table.dataTable thead .sorting_asc_disabled:before,
+table.dataTable thead .sorting_desc:after,
+table.dataTable thead .sorting_desc:before,
+table.dataTable thead .sorting_desc_disabled:after,
+table.dataTable thead .sorting_desc_disabled:before {
+bottom: .5em;
+}
+    
+</style>
 
